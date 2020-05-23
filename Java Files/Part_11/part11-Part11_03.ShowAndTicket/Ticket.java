f56743e122c74634c11c96d2421e351d3072a255
junioj <60082886+junioj@users.noreply.github.com>
2020-05-22 21:15:20 -0400

    11.1

---
+++ Java Files/Part_11/part11-Part11_03.ShowAndTicket/Ticket.java
@@ -0,0 +1,19 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sagar Bhatt
+ */
+public class Ticket {
+    private int seat;
+    private int code;
+    private ArrayList<Show> shows;
+    private Show show;
+}
